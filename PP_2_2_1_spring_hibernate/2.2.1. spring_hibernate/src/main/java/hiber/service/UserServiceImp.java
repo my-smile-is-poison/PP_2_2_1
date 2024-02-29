32398c5381e2be7bb07fdbb7bea666482c689bda
my-smile-is-poison <152069558+my-smile-is-poison@users.noreply.github.com>
2024-02-29 12:06:34 +0300

    Add files via upload

---
+++ PP_2_2_1_spring_hibernate/2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -0,0 +1,35 @@
+package hiber.service;
+
+import hiber.dao.UserDao;
+import hiber.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService {
+
+   @Autowired
+   private UserDao userDao;//todo: неправильно именование слоя ..Spring, Hibernate ..уже repository
+
+   @Transactional//todo: выносим как типовую - над классом
+   @Override
+   public void add(User user) {
+      userDao.add(user);
+   }
+
+   @Transactional(readOnly = true)
+   @Override
+   public List<User> listUsers() {
+      return userDao.listUsers();
+   }
+
+   @Transactional(readOnly = true)
+   @Override
+   public User getUserByCarModelAndSeries(String model, int series) {//todo: пора избавляться от примитивов
+      return userDao.getUserByCarModelAndSeries(model, series);
+   }
+
+}
